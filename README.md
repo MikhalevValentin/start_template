3b2a538d7b7aff98be544d1c2448f59124f7d0f9
MikhalevValentin <79507776649@yandex.ru>
2016-01-11 17:23:17 +0300

    добавил файлы сборки

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # start_template
 start template for  frontend
+Стартовый шаблон для фронтенд
+
+Основа сборки позаимствована <a href="https://github.com/agragregra/start_html">здесь</a>. 
+Большое спасибо автору! 
+
+1. Копируем репозиторий.
+
+2. Устанавливаем Gulp ->  npm install --save-dev gulp
+
+3. Устанавливаем плагины:
+	- npm install --save-dev gulp-autoprefixer
+	- npm install --save-dev gulp-concat-css
+	- npm install --save-dev gulp-livereload
+	- npm install --save-dev gulp-connect
+	- npm install --save-dev gulp-minify-css
+	- npm install --save-dev gulp-notify
+	- npm install --save-dev gulp-rename
+	- npm install --save-dev gulp-sass
+    - npm install --save-dev gulp-uglify
+
+4. Запускаем -> gulp
+5. Livereload ->  localhost:8080
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var prefix = require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+
+//server connect
+gulp.task('connect', function () {
+    connect.server({
+        root: 'app',
+        livereload: true
+    });
+});
+
+//scss
+gulp.task('css', function () {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(prefix('last 25 versions'))
+        .pipe(minifyCSS())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('app/css'))
+        .pipe(notify("CSS compile OK!"))
+        .pipe(connect.reload());
+});
+
+//js
+gulp.task('js', function () {
+    return gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(rename('common.min.js'))
+        .pipe(gulp.dest('app/js'))
+        .pipe(notify("JS compress OK!"))
+        .pipe(connect.reload());
+});
+
+//html
+gulp.task('html', function () {
+    gulp.src('app/index.html')
+        .pipe(notify("HTML change OK!"))
+        .pipe(connect.reload());
+})
+
+//watch
+gulp.task('watch', function () {
+    gulp.watch('scss/*.scss', ['css'])
+    gulp.watch('js/*.js', ['js'])
+    gulp.watch('app/index.html', ['html'])
+})
+
+//default
+gulp.task('default', ['connect', 'html', 'css', 'js', 'watch']);
